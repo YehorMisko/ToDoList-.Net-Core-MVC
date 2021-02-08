10eb8a5547dfd54994900b2d087abf77e35b1583
YehorMisko <jayden@gmail.com>
2021-02-09 01:23:33 +0200

    Создал сайт с функционалом To Do List

---
+++ TodoListforrealthistime/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using Dapper.Contrib.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Helpers;
+using TodoListforrealthistime.Models;
+using TodoListforrealthistime.ViewModels;
+
+namespace TodoListforrealthistime.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            TodoListViewModel viewModel = new TodoListViewModel();
+            return View("Index", viewModel);
+        }
+        public IActionResult Edit(int id)
+        {
+            TodoListViewModel viewModel = new TodoListViewModel();
+            viewModel.EditableItem = viewModel.TodoItems.FirstOrDefault(x => x.Id == id);
+            return View("Index", viewModel);
+        }
+        public IActionResult Delete(int id)
+        {
+            using (var db = DbHelper.GetConnection())
+            {
+                TodoListItem item = db.Get<TodoListItem>(id);
+                if (item != null)
+                    db.Delete(item);
+                return RedirectToAction("Index");
+            }
+        }
+        public IActionResult CreateUpdate(TodoListViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = DbHelper.GetConnection())
+                {
+                    if (viewModel.EditableItem.Id <= 0)
+                    {
+                        viewModel.EditableItem.AddDate = DateTime.Now;
+                        db.Insert<TodoListItem>(viewModel.EditableItem);
+                    }
+                    else
+                    {
+                        TodoListItem dbItem = db.Get<TodoListItem>(viewModel.EditableItem.Id);
+                        var result = TryUpdateModelAsync<TodoListItem>(dbItem, "EditableItem");
+                        db.Update<TodoListItem>(dbItem);
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            else
+                return View("Index", new TodoListViewModel());
+        }
+        public IActionResult ToggleIsDone(int id)
+        {
+            using (var db = DbHelper.GetConnection())
+            {
+                TodoListItem item = db.Get<TodoListItem>(id);
+                if (item != null)
+                {
+                    item.IsDone = !item.IsDone;
+                    db.Update<TodoListItem>(item);
+                }
+                return RedirectToAction("Index");
+            }
+        }
+    
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TodoListforrealthistime/Models/TodoListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListforrealthistime.Models
+{
+    public class TodoListItem
+    {
+        public int Id { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must contain at least two characters!")]
+        [MaxLength(100, ErrorMessage = "Title must contain a maximum of 100 characters!")]
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
